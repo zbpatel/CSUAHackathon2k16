d252c69f4a6e97f0417dddb7ceba6b7b73dedb19
zbpatel <zpatel@berkeley.edu>
2016-11-04 18:44:47 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/zac/spicymeatball/ScreenObject.java
@@ -0,0 +1,20 @@
+package com.example.zac.spicymeatball;
+
+import android.media.Image;
+/**
+ * Created by zac on 11/4/2016.
+ * Copyright (c) Zachary Patel
+ */
+
+public abstract class ScreenObject {
+    private double xPos, yPos;
+    private Image image;
+
+
+    public ScreenObject(int x, int y, Image i){
+
+    }
+
+    
+
+}
